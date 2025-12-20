298f37fe4da17671db25083fb6e361d3a3047fa6
league <karinabriellen@gmail.com>
2025-12-20 12:55:59 -0800

    continue enums

--- src/_03_Intro_to_Enums/StatesOfMatter.java
+++ src/_03_Intro_to_Enums/StatesOfMatter.java
@@ -1,5 +1,15 @@
 package _03_Intro_to_Enums;
 
 public enum StatesOfMatter {
-	SOLID, LIQUID, GAS
+	SOLID, LIQUID, GAS;
+	
+	private int celsiusTemp;
+
+	public int getCelsiusTemp() {
+		return celsiusTemp;
+	}
+
+	public void setCelsiusTemp(int celsiusTemp) {
+		this.celsiusTemp = celsiusTemp;
+	}
 }
